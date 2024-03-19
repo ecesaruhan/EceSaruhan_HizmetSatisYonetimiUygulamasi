e28933b91d4b19ecfbe4e2ba654a66a9d8f8f660
ecesaruhan <148263584+ecesaruhan@users.noreply.github.com>
2024-03-19 08:53:33 +0300

    ProductConfig was done.

--- SalesUp/SalesUp.Data/Concrete/Configs/ProductConfig.cs
+++ SalesUp/SalesUp.Data/Concrete/Configs/ProductConfig.cs
@@ -18,8 +18,83 @@ public class ProductConfig : IEntityTypeConfiguration<Product>
             new Product
             {
                 Id = 1,
-                Name = 
+                Name = "Dove Şampuan",
+                Quantity = 100,
+                Price = 105.90,
+                Url="dove-sampuan"
+            },
+            new Product
+            {
+                Id = 2,
+                Name = "Dove Saç Kremi",
+                Quantity = 120,
+                Price = 70.90,
+                Url="dove-sac-kremi"
+            },
+            new Product
+            {
+                Id = 3,
+                Name = "Dove Saç Maskesi",
+                Quantity = 80,
+                Price = 80.90,
+                Url="dove-sac-maskesi"
+            },
+            new Product
+            {
+                Id = 4,
+                Name = "Dove Saç Bakım Spreyi",
+                Quantity = 150,
+                Price = 40.90,
+                Url="dove-sac-bakim-spreyi"
+            },
+            new Product
+            {
+                Id = 5,
+                Name = "Dove Serum",
+                Quantity = 150,
+                Price = 55.90,
+                Url="dove-serum"
+            },
+            new Product
+            {
+                Id = 6,
+                Name = "Dove Saç Köpüğü",
+                Quantity = 150,
+                Price = 90.90,
+                Url="dove-sac-kopugu"
+            },
+            new Product
+            {
+                Id = 7,
+                Name = "Dove Sprey Deodorant",
+                Quantity = 150,
+                Price = 110.90,
+                Url="dove-sprey-deodorant"
+            },
+            new Product
+            {
+                Id = 8,
+                Name = "Dove Roll-On Deodorant",
+                Quantity = 150,
+                Price = 100.90,
+                Url="dove-rollon-deodorant"
+            },
+            new Product
+            {
+                Id = 9,
+                Name = "Dove Bar Sabun",
+                Quantity = 150,
+                Price = 30.90,
+                Url="dove-bar-sabun"
+            },
+            new Product
+            {
+                Id = 10,
+                Name = "Dove Sıvı Sabun",
+                Quantity = 100,
+                Price = 90.90,
+                Url="dove-sıvı-sabun"
             }
-            )
+        );
     }
 }
--- SalesUp/SalesUp.Entity/Product.cs
+++ SalesUp/SalesUp.Entity/Product.cs
@@ -7,7 +7,7 @@ public class Product
     public int Id { get; set; }
     public string Name { get; set; }
     public int Quantity { get; set; }
-    public decimal Price { get; set; }
+    public double Price { get; set; }
     public string Url { get; set; }
     public string UserId { get; set; }
     public User User { get; set; }
