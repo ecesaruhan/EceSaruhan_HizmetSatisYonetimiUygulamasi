09d852d570054fcaadb7e007cc7ad0e326ca590e
ecesaruhan <148263584+ecesaruhan@users.noreply.github.com>
2024-03-29 20:27:03 +0300

    ServiceCollectionExtensions was completed.
    ServiceCollectionExtensions was created.
    Services were added in to this class.
    Methods were added to program.cs.

--- SalesUp/SalesUp.MVC/Extensions/ServiceCollectionExtension.cs
+++ SalesUp/SalesUp.MVC/Extensions/ServiceCollectionExtension.cs
@@ -1,6 +1,120 @@
+using AspNetCoreHero.ToastNotification;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SalesUp.Business.Abstract;
+using SalesUp.Business.Concrete;
+using SalesUp.Business.Mappings;
+using SalesUp.Data.Abstract;
+using SalesUp.Data.Concrete.Contexts;
+using SalesUp.Data.Concrete.Repositories;
+using SalesUp.Entity.Identity;
+using SalesUp.MVC.EmailServices.Abstract;
+using SalesUp.MVC.EmailServices.Concrete;
+
 namespace SalesUp.MVC.Extensions;
 
-public class ServiceCollectionExtension
+public static class ServiceCollectionExtension
 {
-    
+    public static IServiceCollection LoadMyDbContextServices(this IServiceCollection services)
+    {
+        services.AddDbContext<SalesUpDbContext>(options =>
+            options
+                .UseSqlite(services
+                    .BuildServiceProvider()
+                    .GetRequiredService<IConfiguration>()
+                    .GetConnectionString("SqliteConnection")));
+        return services;
+    }
+
+    public static IServiceCollection LoadMyIdentityServices(this IServiceCollection services)
+    {
+        services.AddIdentity<User, Role>()
+            .AddEntityFrameworkStores<SalesUpDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.Configure<IdentityOptions>(options =>
+        {
+            #region Parola Ayarları
+
+            options.Password.RequiredLength = 6;
+            options.Password.RequireDigit = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireLowercase = true;
+
+            #endregion
+
+            #region Hesap Kilitleme Ayarları
+
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(50);
+            options.Lockout.MaxFailedAccessAttempts = 3;
+
+            #endregion
+
+            options.User.RequireUniqueEmail = true;
+            options.SignIn.RequireConfirmedEmail = false;
+        });
+
+        services.ConfigureApplicationCookie(options =>
+        {
+            options.LoginPath = "/Account/Login";
+            options.LogoutPath = "/";
+            options.AccessDeniedPath = "/Account/AccessDenied";
+            options.ExpireTimeSpan = TimeSpan.FromDays(10);
+            options.SlidingExpiration = true;
+            options.Cookie = new CookieBuilder
+            {
+                Name = "SalesUp.Security.Cookie",
+                HttpOnly = true,
+                SameSite = SameSiteMode.Strict
+            };
+        });
+        return services;
+    }
+
+    public static IServiceCollection LoadMyRepositoryServices(this IServiceCollection services)
+    {
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddScoped<IMessageRepository, MessageRepository>();
+        services.AddScoped<ISTaskRepository, STaskRepository>();
+        services.AddScoped<ISTaskItemRepository, STaskItemRepository>();
+        services.AddScoped<ISaleRepository, SaleRepository>();
+        services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+
+        services.AddScoped<IProductService, ProductManager>();
+        services.AddScoped<ICustomerService, CustomerManager>();
+        services.AddScoped<IMessageService, MessageManager>();
+        services.AddScoped<ISTaskService, STaskManager>();
+        services.AddScoped<ISTaskItemService, STaskItemManager>();
+        services.AddScoped<ISaleService, SaleManager>();
+        services.AddScoped<ISubscriptionService, SubscriptionManager>();
+        services.AddScoped<IOrderService, OrderManager>();
+        return services;
+    }
+
+    public static IServiceCollection LoadMyOtherServices(this IServiceCollection services)
+    {
+        services.AddScoped<MapperlyConfig>();
+        
+        // services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+        services.AddScoped<IEmailSender, EmailSender>(options=>new EmailSender(
+            services.BuildServiceProvider().GetRequiredService<IConfiguration>()["EmailSender:Host"],
+
+            services.BuildServiceProvider().GetRequiredService<IConfiguration>().GetValue<int>("EmailSender:Port"),
+            services.BuildServiceProvider().GetRequiredService<IConfiguration>().GetValue<bool>("EmailSender:EnableSSL"),
+            services.BuildServiceProvider().GetRequiredService<IConfiguration>()["EmailSender:UserName"],
+            services.BuildServiceProvider().GetRequiredService<IConfiguration>()["EmailSender:Password"]
+        ));
+        services.AddNotyf(options =>
+        {
+            options.DurationInSeconds = 3;
+            options.IsDismissable = true;
+            options.Position = NotyfPosition.TopRight;
+        });
+        return services;
+    }
 }
--- SalesUp/SalesUp.MVC/Program.cs
+++ SalesUp/SalesUp.MVC/Program.cs
@@ -17,83 +17,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.LoadMyDbContextServices();
 
-builder.Services.AddDbContext<SalesUpDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnections")));
+builder.Services.LoadMyIdentityServices();
 
+builder.Services.LoadMyRepositoryServices();
 
-builder.Services.AddIdentity<User, Role>()
-    .AddEntityFrameworkStores<SalesUpDbContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    #region Parola Ayarları
-    options.Password.RequiredLength = 6;
-    options.Password.RequireDigit = true;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequireLowercase = true;
-    #endregion
-
-    #region Hesap Kilitleme Ayarları
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(50);
-    options.Lockout.MaxFailedAccessAttempts = 3;
-    #endregion
-
-    options.User.RequireUniqueEmail = true;
-    options.SignIn.RequireConfirmedEmail = false;
-});
-
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    options.LoginPath = "/Account/Login";
-    options.LogoutPath = "/";
-    options.AccessDeniedPath = "/Account/AccessDenied";
-    options.ExpireTimeSpan = TimeSpan.FromDays(10);
-    options.SlidingExpiration = true;
-    options.Cookie = new CookieBuilder
-    {
-        Name = "SalesUp.Security.Cookie",
-        HttpOnly = true,
-        SameSite = SameSiteMode.Strict
-    };
-});
-
-builder.Services.AddScoped<MapperlyConfig>();
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
-builder.Services.AddScoped<IMessageRepository, MessageRepository>();
-builder.Services.AddScoped<ISTaskRepository, STaskRepository>();
-builder.Services.AddScoped<ISTaskItemRepository, STaskItemRepository>();
-builder.Services.AddScoped<ISaleRepository, SaleRepository>();
-builder.Services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-
-builder.Services.AddScoped<IProductService, ProductManager>();
-builder.Services.AddScoped<ICustomerService, CustomerManager>();
-builder.Services.AddScoped<IMessageService, MessageManager>();
-builder.Services.AddScoped<ISTaskService, STaskManager>();
-builder.Services.AddScoped<ISTaskItemService, STaskItemManager>();
-builder.Services.AddScoped<ISaleService, SaleManager>();
-builder.Services.AddScoped<ISubscriptionService, SubscriptionManager>();
-builder.Services.AddScoped<IOrderService, OrderManager>();
-
-builder.Services.AddScoped<IEmailSender, EmailSender>(options=>new EmailSender(
-    builder.Configuration["EmailSender:Host"],
-    builder.Configuration.GetValue<int>("EmailSender:Port"),
-    builder.Configuration.GetValue<bool>("EmailSender:EnableSSL"),
-    builder.Configuration["EmailSender:UserName"],
-    builder.Configuration["EmailSender:Passwor"]
-    ));
-builder.Services.AddNotyf(options =>
-{
-    options.DurationInSeconds = 3;
-    options.IsDismissable = true;
-    options.Position = NotyfPosition.TopRight;
-});
+builder.Services.LoadMyOtherServices();
 
 var app = builder.Build();
 
@@ -112,6 +42,14 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+#region RouteRegions
+
+app.MapAreaControllerRoute(
+    name:"User",
+    areaName:"User",
+    pattern:"User/{controller=Home}/{action=Index}/{id?}"
+);
+
 app.MapAreaControllerRoute(
     name:"Admin",
     areaName:"Admin",
@@ -121,5 +59,7 @@ app.MapAreaControllerRoute(
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+#endregion
 
 app.UpdateDatabase().Run();
