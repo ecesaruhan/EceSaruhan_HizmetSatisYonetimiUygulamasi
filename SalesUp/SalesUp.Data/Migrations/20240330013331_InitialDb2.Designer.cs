d898919ac9d51c0d7a483115ec6acc8b400b90bb
ecesaruhan <148263584+ecesaruhan@users.noreply.github.com>
2024-03-30 04:37:05 +0300

    dotnet ef migrations was done.

--- SalesUp/SalesUp.Data/Migrations/20240330013331_InitialDb2.Designer.cs
+++ SalesUp/SalesUp.Data/Migrations/20240330013331_InitialDb2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesUp.Data.Concrete.Contexts;
 
@@ -10,9 +11,11 @@ using SalesUp.Data.Concrete.Contexts;
 namespace SalesUp.Data.Migrations
 {
     [DbContext(typeof(SalesUpDbContext))]
-    partial class SalesUpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240330013331_InitialDb2")]
+    partial class InitialDb2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.17");
